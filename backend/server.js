b4c673a3e0a84781894bf0e10484151e2c6440b9
Rahull Adk <shanehtetaung.conceptx.mm@gmail.com>
2024-07-01 15:29:23 +0700

    13-ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import { v2 as cloudinary } from "cloudinary";
@@ -8,10 +9,10 @@ import cookieParser from "cookie-parser";
 import userRoutes from "./routes/user.routes.js";
 import postRoutes from "./routes/post.routes.js";
 import notificationRoutes from "./routes/notification.routes.js";
-const app = express();
-const port = process.env.PORT || 4000;
 dotenv.config();
-
+const app = express();
+const port = process.env.PORT || 5000;
+const __dirname = path.resolve();
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
@@ -28,6 +29,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  app.get("*", (_, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 app.listen(port, () => {
   console.log("Server is running on port:", port);
   connectDB();
